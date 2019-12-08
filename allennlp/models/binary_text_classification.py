fc985828a6c9989907ba46c62ac2f9e49f7b74af
berfu <berfu.buyukoz@boun.edu.tr>
2019-12-08 12:49:04 +0300

    Edit scoring function

--- allennlp/models/binary_text_classification.py
+++ allennlp/models/binary_text_classification.py
@@ -48,18 +48,50 @@ class TextClassifier(Model):
         self.output_dict = output_dict
         self.labels = label
 
+    def _confusion(self, truth, predictions):
+        confusion_matrix = torch.zeros(self.args.nclasses, self.args.nclasses)
+        for t, p in zip(truth.view(-1), predictions.view(-1)):
+                confusion_matrix[t.long(), p.long()] += 1
+        
+        tp = confusion_matrix[1,1].item()
+        fp = confusion_matrix[0,1].item()
+        tn = confusion_matrix[0,0].item()
+        fn = confusion_matrix[1,0].item()
+        return tp,fp,tn,fn
+
+    def _get_scores(self, truth, predictions):
+        tp,fp,tn,fn = self._confusion(truth, predictions)
+        prot_recall = tp/(tp+fn) if (tp+fn) else 0
+        prot_precision = tp/(tp+fp) if (tp+fp) else 0
+        prot_f1_score = 2 * prot_precision * prot_recall / (prot_precision + prot_recall) if (prot_precision + prot_recall) else 0
+
+        non_prot_recall = tn/(tn+fp) if (tn+fp) else 0
+        non_prot_precision = tn/(tn+fn) if (tn+fn) else 0
+        non_prot_f1_score = 2 * non_prot_precision * non_prot_recall / (non_prot_precision + non_prot_recall) if (non_prot_precision + non_prot_recall) else 0
+
+        # Take average of two class scores (macro)
+        f1_macro = (prot_f1_score + non_prot_f1_score)/2
+        prec_macro = (prot_precision + non_prot_precision)/2
+        recall_macro = (prot_recall + non_prot_recall)/2
+        acc = (tp + tn)/(tp+fp+tn+fn)
+        return acc,prec_macro,recall_macro,f1_macro, prot_precision, prot_recall, prot_f1_score
+
     def get_metrics(self) -> Dict[str, float]:
         labels = self.labels
-        labels_list = labels.squeeze(-1).cpu().data.numpy()
-        predicted_labels = self.output_dict['predicted_labels'].squeeze(-1).cpu().data.numpy()
+        # labels_list = labels.squeeze(-1).cpu().data.numpy()
+        # predicted_labels = self.output_dict['predicted_labels'].squeeze(-1).cpu().data.numpy()
         # predicted_labels_as_int = [int(l) for l in predicted_labels]
-        acc = accuracy_score(labels_list, predicted_labels)
-        prf = precision_recall_fscore_support(labels_list, predicted_labels, average='macro')
+        # acc = accuracy_score(labels_list, predicted_labels)
+        # prf = precision_recall_fscore_support(labels_list, predicted_labels, average='macro')
+        acc,prec,rec,fmacro, pos_prec, pos_rec, pos_fmacro = self._get_scores(labels, self.output_dict['predicted_labels'])
         metrics = {}
         metrics['acc'] = acc
-        metrics['prec'] = prf[0]
-        metrics['rec'] = prf[1]
-        metrics['fmacro'] = prf[2]
+        metrics['prec'] = prec
+        metrics['rec'] = rec
+        metrics['fmacro'] = fmacro
+        metrics['pos_prec'] = pos_prec
+        metrics['pos_rec'] = pos_rec
+        metrics['pos_fmacro'] = pos_fmacro
         return metrics
 
     def decode(self):
